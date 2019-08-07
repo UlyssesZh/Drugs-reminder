252fe4013b188cd03db0ac84364b5deac710a252
UlyssesZhan <2938747508@qq.com>
2019-08-07 14:29:08 +0800

    Added 'About'

--- app/src/main/java/ulysses/apps/drugsreminder/activities/AboutActivity.java
+++ app/src/main/java/ulysses/apps/drugsreminder/activities/AboutActivity.java
@@ -1,6 +1,20 @@
 package ulysses.apps.drugsreminder.activities;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import ulysses.apps.drugsreminder.R;
 
 public class AboutActivity extends AppCompatActivity {
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about_activity);
+		Toolbar toolbar = findViewById(R.id.about_toolbar);
+		setSupportActionBar(toolbar);
+		toolbar.setNavigationOnClickListener(view -> finish());
+	}
 }
