4147320125fe4b84ee7fe63b43db417e52741cb4
UlyssesZhan <2938747508@qq.com>
2019-08-12 15:26:24 +0800

    Added 'ExceptionCatcher' for debugging

---
+++ app/src/main/java/ulysses/apps/drugsreminder/util/ExceptionCatcher.java
@@ -0,0 +1,45 @@
+package ulysses.apps.drugsreminder.util;
+
+import android.app.AlertDialog;
+import android.content.Context;
+
+import ulysses.apps.drugsreminder.BuildConfig;
+
+public class ExceptionCatcher {
+	public static void catchException(Runnable runnable, OnExceptionCaughtListener listener) {
+		if (BuildConfig.DEBUG)
+			try {
+				runnable.run();
+			} catch (Exception e) {
+				e.printStackTrace();
+				listener.onExceptionCaught(e);
+			}
+		else runnable.run();
+	}
+	public static void catchException(Context context, Runnable runnable) {
+		catchException(runnable, new ExceptionAlerter(context));
+	}
+	public interface OnExceptionCaughtListener {
+		void onExceptionCaught(Exception e);
+	}
+	public static class ExceptionAlerter implements OnExceptionCaughtListener {
+		private Context context;
+		public ExceptionAlerter(Context context) {
+			this.context = context;
+		}
+		@Override
+		public void onExceptionCaught(Exception e) {
+			AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+			StringBuilder stringBuilder = new StringBuilder();
+			stringBuilder.append(e.getClass().getName());
+			stringBuilder.append(": ");
+			stringBuilder.append(e.getMessage());
+			for (StackTraceElement stackTraceElement : e.getStackTrace()) {
+				stringBuilder.append("\n");
+				stringBuilder.append(stackTraceElement.toString());
+			}
+			alertBuilder.setMessage(stringBuilder.toString());
+			alertBuilder.create().show();
+		}
+	}
+}
